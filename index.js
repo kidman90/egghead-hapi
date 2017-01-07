15add9daa32d04bb3b849694838e1404ae4de914
Wai-Yin Kwan <wykhuh@icloud.com>
2017-01-07 12:23:44 -0800

    show  custom error page

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Hapi = require('hapi');
+const Boom = require('boom');
 
 // setup server
 const server = new Hapi.Server();
@@ -24,17 +25,43 @@ server.register([
     register: require('good'),
     options,
   },
+  {
+    register: require('vision'),
+  },
 ],
 // register takes callback as the last argument
 (err) => {
   if (err) { return console.error(err); }
 
-  // every route needs method, path, handler
+  // setup view engine
+  server.views({
+    engines: {
+      hbs: require('handlebars'),
+    },
+    // location of views
+    relativeTo: __dirname,
+    path: 'views',
+  });
+
+  server.ext('onPreResponse', (request, reply) => {
+    const resp = request.response;
+    // if there is an error, hapi returns a Boom error object and isBoom is true
+
+    // if there isn't a Boom object, continue
+    if (!resp.isBoom) return reply.continue();
+
+    // else there is a Boom object.
+    reply.view('error', resp.output.payload)
+      // must set the statusCode, otherwise it will return 200 for rendering
+      // the error page file
+      .code(resp.output.statusCode);
+  });
+
   server.route({
     method: 'GET',
     path: '/',
     handler: (request, reply) => {
-      reply('hello hapi');
+      reply(Boom.notFound());
     },
   });
 
