0811564765773aa6e01037176760f160939a144c
Wai-Yin Kwan <wykhuh@icloud.com>
2017-01-07 12:11:19 -0800

    lifecycle events

--- index.js
+++ index.js
@@ -29,11 +29,67 @@ server.register([
 (err) => {
   if (err) { return console.error(err); }
 
+  // === lifecycle events ===
+
+  // as soon as request is recieved; before it has passed through router
+  server.ext('onRequest', (request, reply) => {
+    console.log('onRequest');
+    // turn every request into GET on '/'
+    request.setUrl('/');
+    request.setMethod('GET');
+    reply.continue();
+  });
+
+  // request goes through the router
+
+  // after request has gone through the router, before authentication
+  server.ext('onPreAuth', (request, reply) => {
+    console.log('onPreAuth');
+    reply.continue();
+  });
+
+  // authentication
+
+  // after authentication
+  server.ext('onPostAuth', (request, reply) => {
+    console.log('onPostAuth');
+    reply.continue();
+  });
+
+  // validation
+
+  // after validation, before handler runs
+  server.ext('onPreHandler', (request, reply) => {
+    console.log('onPreHandler');
+    reply.continue();
+  });
+
+  // handler runs
+
+  // after handler runs
+  server.ext('onPostHandler', (request, reply) => {
+    console.log('onPostHandler');
+    reply.continue();
+  });
+
+  // response payload is validated
+
+  // after response payload is validated
+  server.ext('onPreResponse', (request, reply) => {
+    console.log('onPreResponse');
+    reply.continue();
+  });
+
+  // send response to client
+
+  // === lifecycle events ===
+
   // every route needs method, path, handler
   server.route({
     method: 'GET',
     path: '/',
     handler: (request, reply) => {
+      console.log('handler');
       reply('hello hapi');
     },
   });
