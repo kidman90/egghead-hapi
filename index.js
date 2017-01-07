2851361ad569717f1d1c9ff647b5e2189edc746b
Wai-Yin Kwan <wykhuh@icloud.com>
2017-01-07 11:07:28 -0800

    add view engine and hbs templates

--- index.js
+++ index.js
@@ -24,19 +24,43 @@ server.register([
     register: require('good'),
     options,
   },
+  {
+    register: require('vision'),
+  },
 ],
 // register takes callback as the last argument
 (err) => {
   if (err) { return console.error(err); }
 
-  // every route needs method, path, handler
+  // setup view engine
+  server.views({
+    engines: {
+      hbs: require('handlebars'),
+    },
+    // location of views
+    relativeTo: __dirname,
+    path: 'views',
+    // allow the use of layout templates
+    layout: true,
+  });
+
   server.route({
     method: 'GET',
     path: '/',
     handler: (request, reply) => {
-      reply('hello hapi');
+      reply.view('home');
     },
   });
+
+  server.route({
+    method: 'GET',
+    path: '/hi/{name?}',
+    handler: (request, reply) => {
+      // pass object to send data to template
+      reply.view('home', { name: request.params.name || 'World' });
+    },
+  });
+
 
   // start server
   server.start(() => console.log(`Started at: ${server.info.uri}`));
